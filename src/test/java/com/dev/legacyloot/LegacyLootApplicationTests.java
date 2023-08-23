7635f4bcbc26417dff9d3bf80c25f148b2b3709e
hercules <aashish2535@gmail.com>
2023-08-23 13:47:35 +0530

    Initial Commit

---
+++ src/test/java/com/dev/legacyloot/LegacyLootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.legacyloot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyLootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
